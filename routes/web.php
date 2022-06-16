448c6a104a8875ac666ac7519f4c5d25b31370c9
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-06-16 10:35:08 +0200

    remove middleware

--- routes/web.php
+++ routes/web.php
@@ -42,5 +42,5 @@ Route::namespace('Guest')
 
 Route::delete('/picture/{id}', 'User\PicturesDeleteController@destroy')->name("picture.destroy");
 
-Route::resource('/apartment',"User\ApartmentController")->middleware('auth');
+Route::resource('/apartment',"User\ApartmentController");
 Route::delete('/apartment/{id}', 'User\ApartmentController@destroy')->name('user.apartment.destroy');
