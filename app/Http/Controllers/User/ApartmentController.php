81fbb13e5a63c750f29e7dd965a82670f12e6683
Alessio1921 <caruso.alessio1999@gmail.com>
2022-06-16 15:37:21 +0200

    add validate city

--- app/Http/Controllers/User/ApartmentController.php
+++ app/Http/Controllers/User/ApartmentController.php
@@ -77,7 +77,7 @@ class ApartmentController extends Controller
         // $response = Http::get('https://api.tomtom.com/search/2/search' . $newAddress . '.json?storeResult=false&view=Unified&key='.env("APP_KEYMAPS"));
 
         $dataResponse = json_decode($response->body(), true);
-        if(strtolower($dataResponse["results"][0]["address"]["streetName"]) == strtolower($data["address"]) )
+        if((strtolower($dataResponse["results"][0]["address"]["streetName"]) == strtolower($data["address"])) && (strtolower($dataResponse["results"][0]["address"]["localName"]) == strtolower($data["address_city"])))
         {
             if($request['visible'] != null){
                 $data['visible'] = 1;
