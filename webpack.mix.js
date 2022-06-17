c21b9f343f19b75a178f27c475b82b2b6d2fe853
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-06-17 11:55:44 +0200

    add fontawesome

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .options({ processCssUrls : false });
+    .options({ processCssUrls : false })
+    .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts');
