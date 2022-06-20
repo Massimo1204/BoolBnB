4537d1b40718bb8ff180279a69158415f4cc8848
Ashour100 <interinstitute5@gmail.com>
2022-06-20 15:43:46 +0200

    View Modification

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
         <h1>Hey</h1>
+        <Main/>
     </div>
 </template>
 
 <script>
+import Main from '../components/Main';
 export default {
-    name:"App"
+    name:"App",
+    components:{
+        Main,
+    }
 }
 </script>
 
