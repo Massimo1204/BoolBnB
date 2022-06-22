bc42f33fa97614d462d67e36ac6d11756377802c
Alessio Caruso <caruso.alessio1999@gmail.com>
2022-06-22 16:25:16 +0200

    modify search

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,23 +1,30 @@
 <template>
-<div>
+<div class="container">
     <div class="row">
-        <div class="col-4 search">
-            <label class="input-container closed">
-                <div class="shadow"></div>
-                <div class="center">
-                    <input type="text" class="input" placeholder="Search" @keyup.enter="search" v-model="userSearch">
-                </div>
-                <div class="sticks" ></div>
-            </label>
+        <div class="col-12 search">
+            <div class="search">
+							<input type="text" placeholder=" " @keyup.enter="search" v-model="userSearch">
+							<div>
+									<svg>
+											<use xlink:href="#path"></use>
+									</svg>
+							</div>
+					</div>
+							
+					<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+							<symbol xmlns="http://www.w3.org/2000/svg" viewBox="0 0 160 28" id="path">
+									<path d="M32.9418651,-20.6880772 C37.9418651,-20.6880772 40.9418651,-16.6880772 40.9418651,-12.6880772 C40.9418651,-8.68807717 37.9418651,-4.68807717 32.9418651,-4.68807717 C27.9418651,-4.68807717 24.9418651,-8.68807717 24.9418651,-12.6880772 C24.9418651,-16.6880772 27.9418651,-20.6880772 32.9418651,-20.6880772 L32.9418651,-29.870624 C32.9418651,-30.3676803 33.3448089,-30.770624 33.8418651,-30.770624 C34.08056,-30.770624 34.3094785,-30.6758029 34.4782612,-30.5070201 L141.371843,76.386562" transform="translate(83.156854, 22.171573) rotate(-225.000000) translate(-83.156854, -22.171573)"></path>
+							</symbol>
+					</svg>
         </div>
     </div>
     <div class="row">
 		<div class="col-12">
-			<h1 class="ms-5 mt-4">In evidenza</h1>
+			<h1 class="mt-4">In evidenza</h1>
 		</div>
         <SingleApartment v-for="(apartment,index) in sponsoredApartments" :key="'sponsored'+ index" :apartment="apartment" />
 		<div class="col-12">
-			<h1 class="ms-5">Normali</h1>
+			<h1 >Normali</h1>
 		</div>
         <SingleApartment v-for="(apartment,index) in filterApartments" :key="index" :apartment="apartment" />
         <div class="col-12 d-flex justify-content-between my-3" v-if="apartmentsSearch == ''">
@@ -114,161 +121,100 @@ export default {
 <style scoped lang="scss">
 @import "../../sass/_variables.scss";
 
-.search
-{
-	--height: 50;
-	--width: 300;
-	--border: 5;
-	--speed: 0.4;
-	--ease: cubic-bezier(.85,.01,.4,.97);
-	// --color-bk: white; //#547AA5;
-	
-	background: var(--color-bk);
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	
-	transition-property: background;
-	transition-duration: calc(var(--speed) * 1s);
-	transition-timing-function: var(--ease);
-	
-	&:focus-within {
-		--speed: 0.7;
-		// background-color: white; //#456488;
-	 }
+$color: $primary;
+
+.search {
+    display: table;
+    // img { height: 2em; }
+    input {
+        background: none;
+        border: none;
+        outline: none;
+        width: 25px;
+        min-width: 0;
+        padding: 0;
+        z-index: 1;
+        position: relative;
+        line-height: 18px;
+        margin: 5px 0;
+        font-size: 25px;
+        -webkit-appearance: none;
+        font-family: 'Mukta Malar';
+        transition: all .6s ease;
+        cursor: pointer;
+        color: $color;
+        & + div {
+            position: relative;
+            height: 28px;
+            width: 100%;
+            margin: -28px 0 0 0;
+            svg {
+                display: block;
+                position: absolute;
+                height: 28px;
+                width: 160px;
+                right: 0;
+                top: 0;
+                fill: none;
+                stroke: $color;
+                stroke-width: 1.5px;
+                stroke-dashoffset: 212.908 + 59;
+                stroke-dasharray: 59 212.908;
+                transition: all .6s ease;
+            }
+        }
+        &:not(:placeholder-shown),
+        &:focus {
+            width: 160px;
+            padding: 0 4px;
+            cursor: text;
+            &+ div {
+                svg {
+                    stroke-dasharray: 150 212.908;
+                    stroke-dashoffset: 300;
+                }
+            }
+        }
+    }
 }
 
-::placeholder { color: rgba($primary, 0.5); }
-:-ms-input-placeholder { color: rgba($primary, 0.5);}
-::-ms-input-placeholder { color: rgba($primary, 0.5);}
+// ::selection {
+//     background: rgba(#fff, .2);
+// }
+// ::-moz-selection {
+//     background: rgba(#fff, .2);
+// }
 
-.input-container
-{
-	width: calc(var(--width) * 1px);
-	height: calc(var(--height) * 1px);
-	position: relative;
-	cursor: pointer;
-	transform: rotate(12deg) scale(0.7);
-	
-	transition-property: opacity, transform;
-	transition-duration: calc(var(--speed) * 1s);
-	transition-timing-function: var(--ease);
-	
-	.center
-	{
-		border: calc(var(--border) * 1px) solid $primary;
-		border-left: none;
-		border-right: none;
-		width: 100%;
-		height: calc(100% - var(--border) * 2px);
-		transform: scalex(0);
-		transition: inherit;
-		background-color: var(--color-bk);
-	}
-	
-	input
-	{
-		transition: inherit;
-		width: calc(100% - var(--height));
-		height: 100%;
-		border: 0;
-		outline: 0;
-		color: $primary;
-		background: transparent;
-		font-size: 1.3rem;
-		opacity: 0;
-		padding: 0;
-		margin: 0;
-	}
-	
-	// .shadow
-	// {
-	// 	position: absolute;
-	// 	width: 100%;
-	// 	height: 100%;
-	// 	border-radius: 2em;
-	// 	top: 0;
-	// 	left: 0;
-	// 	// box-shadow: 0px 10px 50px 0px rgba(0,0,0,0.1);
-	// 	transition: inherit;
-	// 	transform: scalex(0) translateY(-10px);
-	// 	opacity: 0;
-	// }
-	
-	&:after, &:before
-	{
-		z-index:1;
-		content: '';
-		transition: inherit;
-		width: calc((var(--height) * 0.5px) - (var(--border) * 1px));
-		height: calc((var(--height) * 1px) - (var(--border) * 2px));
-		display: block;
-		top: 0;
-		border-color: $primary;
-		position: absolute;
-		background-color: var(--color-bk);
-		border: calc(var(--border) * 1px) solid $primary;
-	}
-	
-	&:before
-	{
-		right: 100%;
-		border-radius: 2em 0 0 2em;
-		border-right: none;
-		transform: translateX(calc(var(--width) * 0.5px));
-	}
-	
-	&:after
-	{
-		left: 100%;
-		border-radius: 0 2em 2em 0;
-		border-left: none;
-		transform: translateX(calc(var(--width) * -0.5px));
-	}
-	
-	.sticks
-	{
-		position: absolute;
-		height: calc(var(--height) * 0.5px);
-		width: 1px;
-		bottom: 0;
-		right: 0;
-		transition: inherit;
-		transition-duration: calc(var(--speed) * 1.25s);
-		transform: translateX(calc((var(--width) - var(--height)) * -0.53px)) rotate(-45deg);
-		z-index: 2;
-		
-		&:before, &:after
-		{
-			transition: transform calc(var(--speed) * 1s) var(--ease);
-			height: calc(var(--height) * 0.5px);
-			width: calc(var(--border) * 1px);
-			position: absolute;
-			content: '';
-			background-color: $primary;
-			left: calc(var(--border) * -0.5px);
-			bottom: calc(var(--height) * -0.25px);
-		}
-	}
-		
-	&:focus-within
-	{
-		transform: rotate(0deg) translatey(-10px);
-		.center{ transform: scalex(1); }
-		.shadow{ transform: scalex(1) translateY(0px); opacity: 1; }
-		input{ opacity: 1; }
-		&:before { transform: translateX(0); }
-		&:after { transform: translateX(0); }
-		
-		.sticks
-		{
-			transition-duration: calc(var(--speed) * 1s);
-			transform: translateX(calc(var(--height) * -0.1px)) translateY(0) rotate(180deg);
-			&:before { transform: rotate(-45deg); }
-			&:after { transform: rotate(45deg); }
-		}
-	}
-	
-	
-}
+// html {
+//     box-sizing: border-box;
+//     -webkit-font-smoothing: antialiased;
+// }
+
+// * {
+//     box-sizing: inherit;
+//     &:before,
+//     &:after {
+//         box-sizing: inherit;
+//     }
+// }
+
+// Center & dribbble
+// body {
+//     min-height: 100vh;
+//     font-family: 'Mukta Malar', Arial;
+//     background: #F3628D;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     .dribbble {
+//         position: fixed;
+//         display: block;
+//         right: 24px;
+//         bottom: 24px;
+//         img {
+//             display: block;
+//             width: 76px;
+//         }
+//     }
+// }
 </style>
