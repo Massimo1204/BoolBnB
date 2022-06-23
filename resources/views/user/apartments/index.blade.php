ddc0a8790912a0a157b6fabcf112316cf72c6a8b
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-06-23 10:58:23 +0200

    update style

--- resources/views/user/apartments/index.blade.php
+++ resources/views/user/apartments/index.blade.php
@@ -6,32 +6,72 @@
             {{ session('deleted-message') }}
         </div>
     @endif
-    <div class="container">
-        <div class="row">
-            <div class="col-12 mb-5">
-                <a href="{{ route('apartment.create') }}" class=" text-black">Aggiungi nuovo Appartmanento <i class="fa-solid fa-plus"></i> </a>
+    <div class="add-apartment-jumbo">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 py-5 d-flex align-items-center text-white justify-content-between">
+                    <div class="title-add-apartment">
+                        <h2>BoolBnb - Host</h2>
+                        <p>Aggiungi nuovi annunci o gestisci quelli già presenti</p>
+                    </div>
+                    <a href="{{ route('apartment.create') }}" class="btn btn-light">Crea annuncio</a>
+                </div>
             </div>
         </div>
     </div>
     @if ($apartments->total())
-        <div class="d-flex flex-wrap justify-content-center">
-            <div class="col-12 text-center">
-                <h1>I tuoi appartamenti</h1>
-            </div>
-            @foreach ($apartments as $apartment)
-                <div class="apartment-wrapper mx-3">
-                    <div class="card">
-                        <a href="{{ route('apartment.show', $apartment) }}">
-                            @if (str_starts_with($apartment->image, 'https://') || str_starts_with($apartment->image, 'http://'))
-                                <img class="border border-rounded" src="{{ $apartment->image }}" alt="apartment">
-                            @else
-                                <img class="border border-rounded" src="{{ asset('/storage') . '/' . $apartment->image }}"
-                                    alt="apartment">
-                            @endif
-                        </a>
+        <div class="container py-5">
+            <div class="row py-5 justify-content-between">
+                @foreach ($apartments as $apartment)
+                    <div class="col-6 d-flex justify-content-between my-apartments">
+                        <div class="apartment-img">
+                            <a href="{{ route('apartment.show', $apartment) }}">
+                                @if (str_starts_with($apartment->image, 'https://') || str_starts_with($apartment->image, 'http://'))
+                                    <img src="{{ $apartment->image }}" alt="apartment">
+                                @else
+                                    <img src="{{ asset('/storage') . '/' . $apartment->image }}" alt="apartment">
+                                @endif
+                            </a>
+                        </div>
+                        <div class="apartment-info">
+                            <h3>{{ $apartment->title }}</h3>
+                            <h5>{{ $apartment->address }}</h5>
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                    <div class="col-6 d-flex justify-content-start align-items-start p-5">
+                        <div class="btn btn-primary me-2">
+                            <a class="text-white text-decoration-none"
+                                href="{{ route('apartment.edit', $apartment) }}">Modifica</a>
+                        </div>
+                        <div class="me-2">
+                            <a type="button" class="btn btn-success"
+                                href="{{ route('sponsorship.index', $apartment) }}">Attiva sponsorizzazione</a>
+                        </div>
+                        <form action="{{ route('apartment.destroy', $apartment->id) }}" method="POST"
+                            class="apartment-form-destroyer me-2" apartment-title="{{ $apartment->title }}">
+                            @csrf
+                            @method('DELETE')
+
+                            <button type="submit" class="btn btn-danger text-white">Elimina</a>
+                        </form>
+                    </div>
+                @endforeach
+            </div>
         </div>
     @endif
+@endsection
+
+@section('footer-scripts')
+    <script defer>
+        const deleteForm = document.querySelector('.apartment-form-destroyer');
+
+        deleteForm.addEventListener('submit', function(event) {
+            event.preventDefault(); // § blocchiamo l'invio del form
+            userConfirmation = window.confirm(
+                `Sei sicuro di voler eliminare ${this.getAttribute('apartment-title')}?`);
+            if (userConfirmation) {
+                this.submit();
+            }
+        });
+    </script>
 @endsection
